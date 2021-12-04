51f5d576422325e812ac8093076d82c26ba799cf
TeenaKohli <teena.kohli.8622@gmail.com>
2021-12-04 10:34:15 +0000

    adding updated data

--- store/views.py
+++ store/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from . models import Product
 
 # Create your views here.
+
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all().filter()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'store/store.html', context)
+
+def product_detail(request):
+    return render(request,'store/product_detail.html')
